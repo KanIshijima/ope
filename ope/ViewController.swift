6a6099c3b5905bdcfe146e4900e70d2e0c195805
aozora <siromon@ezweb.ne.jp>
2016-10-05 22:37:33 +0900

    Initial Commit

---
+++ ope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ope
+//
+//  Created by 石嶋 漢 on 2016/10/05.
+//  Copyright © 2016年 KanIshijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
